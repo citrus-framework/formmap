5b08476635a0f0c1a2d57d3a9c633d47bce0e9ac
kouhei.takemoto <take64@citrus.tk>
2020-06-28 22:36:45 +0900

    メッセージストッカーを利用する

---
+++ src/Formmap/FormmapMessages.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright   Copyright 2020, CitrusFormmap. All Rights Reserved.
+ * @author      take64 <take64@citrus.tk>
+ * @license     http://www.citrus.tk/
+ */
+
+namespace Citrus\Formmap;
+
+use Citrus\Variable\Stockers;
+
+/**
+ * フォームマップのメッセージストック
+ */
+class FormmapMessages extends Stockers
+{
+}
